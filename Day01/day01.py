649ae2e9cebaa28298efbe83dfa14f0cc9862911
MrMinemeet <alex@wtf-my-code.works>
2022-12-02 07:43:34 +0100

    Beautified day01

--- Day01/day01.py
+++ Day01/day01.py
@@ -2,30 +2,39 @@ from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2022, day=1)
 
-raw_data = puzzle.input_data
-
 # Store raw data in a file
 with open("Day01/puzzle.txt", "w") as f:
-    f.write(raw_data)
+    f.write(puzzle.input_data)
+
+def load(data):
+    newData = []
+    for line in data:
+        newData.append(line.strip())
+
+    return newData
+
+def getCals(data):
+    sums = []
+    curSum = 0
+    for i, line in enumerate(data):
+        if line == '' or i == (len(data)-1):
+            sums.append(curSum)
+            curSum = 0
+        else:
+            curSum += int(line)
+
+    return sums
+
+def part1(data):
+    print("Part one:",max(getCals(data))) # 66487
+
+def part2(data):
+    calories = getCals(data)
+    calories.sort(reverse=True)
+    print("Part two:", sum(calories[:3])) # 197301
 
 
-data = raw_data.split("\n")
 
-for i, d in enumerate(data):
-    data[i] = data[i].strip()
-
-
-sums = []
-curSum = 0
-for i, line in enumerate(data):
-    if line == '' or i == (len(data)-1):
-        sums.append(curSum)
-        curSum = 0
-    else:
-        curSum += int(line)
-
-print("Part one:",max(sums)) # 66487
-
-sums.sort(reverse=True)
-print("Part two:", sum(sums[:3])) # 197301
-
+if __name__ == "__main__":
+    part1(load(puzzle.input_data.splitlines()))
+    part2(load(puzzle.input_data.splitlines()))
